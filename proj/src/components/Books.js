53e37e3420d2e4f6c9146017ddc4e3bbd26c5de4
Souad Yakubu <sny2@calvin.edu>
2024-11-03 21:34:34 -0500

    resolve merge conflicts

--- proj/src/components/Books.js
+++ proj/src/components/Books.js
@@ -15,3 +15,25 @@ const books = [
 ];
 
 export default books;
+
+
+// const books = [
+//     {
+//         title: "Practice of the Presence of God",
+//         author: "Brother Lawrence",
+//         link: "https://ccel.org/ccel/lawrence/practice/practice.i.html",
+//         cover: "path",
+//         scriptureReference: "genesis", // Example book reference for API
+//         totalChapters: 50 // Adjust to the actual chapter count
+//     },
+//     {
+//         title: "The Normal Christian Life",
+//         author: "Watchman Nee",
+//         link: "https://ccel.org/ccel/nee/normal/normal.i.html",
+//         cover: "path",
+//         scriptureReference: "matthew",
+//         totalChapters: 28
+//     }
+// ];
+
+// export default books;
